f83cc9092a9eb7adb1849dd7479ef194685dc5c4
Albert Carter <albert.r.carter@gmail.com>
2016-03-14 11:11:39 -0400

    update alarm to use rgb values

--- fade.ino
+++ fade.ino
@@ -1,70 +1,70 @@
-#include <FastLED.h>
 #include <Time.h>
 #include <TimeAlarms.h>
 
-// #define WAKEUP_TIME 3*60*1000 // 30 minutes
+#define CURRENT_HOUR 8
+#define CURRENT_MINUTE 30
+#define CURRENT_DAY 1
+#define CURRENT_MONTH 1
+#define CURRENT_YEAR 2016
 
-#define WAKEUP_TIME 1800000 // 30 minutes
-#define WAKEUP_TIME 18000 // 30 minutes
+#define WAKEUP_HOUR 8
+#define WAKEUP_MINUTE 30
+#define WAKEUP_DURATION 30L*60*1000 // 30 minutes
+// #define WAKEUP_DURATION 3000
 
-#define UPDATE_PERIOD 100 // .01 second , use even numbers
+#define UPDATE_PERIOD 1000 // 1 second
 
 // color at sunrise
-#define START_R 182;
-#define START_G 126;
-#define START_B 91;
-#define START_W 0;
-#define START_BRIGHT 0.0;
+#define START_R 182
+#define START_G 126
+#define START_B 91
 
 // color at noon
-#define END_R 255;
-#define END_G 254;
-#define END_B 230;
-#define END_W 0;
-#define END_BRIGHT 1.0;
+#define END_R 255
+#define END_G 254
+#define END_B 230
 
+#define R_SLOPE ((double)END_R - START_R) / WAKEUP_DURATION
+#define G_SLOPE ((double)END_G - START_G) / WAKEUP_DURATION
+#define B_SLOPE ((double)END_B - START_B) / WAKEUP_DURATION
 
-#define START_SATURATION 255
-#define START_HUE 0
-#define START_VALUE 0
-
-#define END_SATURATION 20
-#define END_HUE 64
-#define END_VALUE 255
-
+#define R_INTERCEPT -((double)START_R / R_SLOPE)
+#define G_INTERCEPT -((double)START_G / G_SLOPE)
+#define B_INTERCEPT -((double)START_B / B_SLOPE)
 
 #define REDPIN 3
 #define GREENPIN 10
 #define BLUEPIN 6
 
-CHSV color;
-int start_time = millis();
+long start_time = 0;
+long first_intercept = 0;
 bool alarmOn = false;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600); 
 
   pinMode(REDPIN,   OUTPUT);
   pinMode(GREENPIN, OUTPUT);
   pinMode(BLUEPIN,  OUTPUT);
 
-  write();
+  write(0, 0, 0);
 
-  color.saturation = START_SATURATION;
-  color.hue = START_HUE;
-  color.value = START_VALUE;
+  // compute the crack of dawn (using setTime as a lousy hack to construct a time_t)
+  setTime(WAKEUP_HOUR, WAKEUP_MINUTE, 0, CURRENT_DAY, CURRENT_MONTH, CURRENT_YEAR);
+  first_intercept = (int)min(R_INTERCEPT, min(G_INTERCEPT, B_INTERCEPT));
+  time_t dawn = now() + 1000 * first_intercept;
+  // set the clock to where it should be
+  setTime(CURRENT_HOUR, CURRENT_MINUTE, 0, CURRENT_DAY, CURRENT_MONTH, CURRENT_YEAR);
 
-
-  setTime(8,29,55,1,1,11); // set time to Saturday 8:29:55am Jan 1 2011
   // create the alarms 
-  Alarm.alarmRepeat(8,30,00, startAlarm);  // 8:30am every day
-  // turn off alarm after 2 hr
-  Alarm.alarmRepeat(8,30,30, stopAlarm);  // 8:30am every day
+  // Alarm.alarmRepeat(hour(dawn), minute(dawn), 00, startAlarm);
+  // Alarm.alarmRepeat(hour(dawn), minute(dawn), 00, startAlarm);
+  Alarm.alarmRepeat(hour(8), minute(30), 10, startAlarm);
   Serial.print("setup complete");
 }
 
 void startAlarm(){
+  start_time = millis();
   alarmOn = true;
   Serial.print("\n--ALARM STARTED--\n");
 }
@@ -72,84 +72,30 @@ void startAlarm(){
 void stopAlarm(){
   Serial.print("\n--STOP ALARM--\n");
   alarmOn = false;
+  write(0, 0, 0);
+}
 
-
-
-  color.saturation = START_SATURATION;
-  color.hue = START_HUE;
-  color.value = START_VALUE;
-  write();
-  }
-
-void write() {
-
-
-  analogWrite(REDPIN, value);
-
-
-
-
-
-
-  CRGB rgb;
-  hsv2rgb_rainbow(color, rgb);
-
-  analogWrite(REDPIN,   rgb.r );
-  analogWrite(GREENPIN, rgb.g );
-  analogWrite(BLUEPIN,  rgb.b );
+void write(char r, char g, char b) {
+  analogWrite(REDPIN,   r);
+  analogWrite(GREENPIN, g);
+  analogWrite(BLUEPIN,  b);
 }
 
 void runAlarm() {
   Serial.print("\nrunalarm\n");
   long elapsed_time = millis() - start_time;
-  float percent_passed = (float)elapsed_time / (float)WAKEUP_TIME;
-  
-  // if percent passed is negative or greater than one,
-  // just keep the brightness up
-  if (abs(percent_passed) > 1.0 ){
-    percent_passed = 1.0;
-  }
-
-  color.hue = (END_HUE - START_HUE) * percent_passed;
-  color.saturation = (START_SATURATION - END_SATURATION) * (1-percent_passed);
-  color.value = (END_VALUE - START_VALUE) * percent_passed;
-
-  // Saturation mins out at specified value
-  if (color.saturation < END_SATURATION) {
-    color.saturation = END_SATURATION;
+  if (elapsed_time < WAKEUP_DURATION - first_intercept) {
+    write( (char)max((R_SLOPE * elapsed_time + start_time - R_SLOPE), 0),
+           (char)max((G_SLOPE * elapsed_time + start_time - G_SLOPE), 0),
+           (char)max((B_SLOPE * elapsed_time + start_time - B_SLOPE), 0));
+  } else {
+    stopAlarm();
   }
 }
 
 void loop() {
-
   if(alarmOn){
     runAlarm();  
-    // Serial.print("\n--ALARM RUNNING---\n");
-  } else {
-    // Serial.print("\n--ALARM NOT RUNNING---\n");
   }
-  
-  // put your main code here, to run repeatedly:
-  // Serial.print("NO FORMAT\n");
-
-  
-  // Serial.print("hue: ");
-  // Serial.print(color.hue);
-  // Serial.print("|");
-  // Serial.print(" saturation: ");
-  // Serial.print(color.saturation);
-  // Serial.print("|");
-  // Serial.print(" value: ");
-  // Serial.print(color.value);
-  // Serial.print("|");
-  // Serial.print(" pct passed: ");
-  // Serial.print("=");
-  // // Serial.print(elapsed_time);
-  // Serial.print("/");
-  // Serial.print(WAKEUP_TIME);
-  // Serial.print("\n---\n");
-
-  write();
   Alarm.delay(UPDATE_PERIOD);
-
 }
